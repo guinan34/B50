34877ca1207c1bb7a122d947750a3a50f8c65a83
黄贵楠 <66418500@qq.com>
2019-09-29 23:05:03 +0800

    bug fix

--- app/Http/Controllers/indexCtl.php
+++ app/Http/Controllers/indexCtl.php
@@ -23,22 +23,53 @@ class indexCtl extends Controller
     	$a=-1;
 
         $songdb=DB::select("
-            SELECT F1.song, F1.actress, F1.type, IFNULL(SUM(F1.amount), 0) AS amount
+            SELECT *
             FROM
             (
-                SELECT S.song, S.type, S.actress, P.project_name, P.amount
-                FROM songs AS S
-                LEFT JOIN
+                SELECT F1.song, F1.actress, F1.type, IFNULL(SUM(F1.amount), 0) AS amount
+                FROM
                 (
-                    SELECT song_id, project_name, amount 
-                    FROM projects
-                    WHERE is_obsolete = 0
-                ) AS P ON S.id = P.song_id
-            ) AS F1
-            GROUP BY F1.song, F1.actress, F1.type
-            ORDER BY F1.type, amount DESC;
-
-        ");
+                    SELECT S.song, S.type, S.actress, P.project_name, P.amount
+                    FROM 
+                    (
+                        SELECT id, song, actress, type 
+                        FROM songs
+                        WHERE type = '队歌'
+                    ) AS S
+                    LEFT JOIN
+                    (
+                        SELECT song_id, project_name, amount 
+                        FROM projects
+                        WHERE is_obsolete = 0
+                    ) AS P ON S.id = P.song_id
+                ) AS F1
+                GROUP BY F1.song, F1.actress, F1.type
+                ORDER BY amount DESC
+            ) AS M1
+            UNION
+            SELECT *
+            FROM
+            (
+                SELECT F1.song, F1.actress, F1.type, IFNULL(SUM(F1.amount), 0) AS amount
+                FROM
+                (
+                    SELECT S.song, S.type, S.actress, P.project_name, P.amount
+                    FROM 
+                    (
+                        SELECT id, song, actress, type 
+                        FROM songs
+                        WHERE type = 'Unit' OR type = 'Solo'
+                    ) AS S
+                    LEFT JOIN
+                    (
+                        SELECT song_id, project_name, amount 
+                        FROM projects
+                        WHERE is_obsolete = 0
+                    ) AS P ON S.id = P.song_id
+                ) AS F1
+                GROUP BY F1.song, F1.actress, F1.type
+                ORDER BY amount DESC
+            ) AS M2");
 
         //return var_dump($songdb);
         $songdb=collect($songdb)->map(function($x){ return (array) $x; })->toArray();
